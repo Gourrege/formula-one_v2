1c79fa8363ceab93547c514f38be994ed64c7c1e
Gourrege <s00259704@atu.ie>
2025-05-12 21:40:05 +0100

    Last push before enviroment

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
